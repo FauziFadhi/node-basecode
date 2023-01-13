028df0dbb1d784cc157391791c2821b0e149f3af
Fauzi Fadhillah <fauzifadhi@rollingglory.com>
2023-01-14 00:10:50 +0700

    ref(model): improve model interface

--- src/utils/base-class/base.interface.ts
+++ src/utils/base-class/base.interface.ts
@@ -1,3 +1,6 @@
+import { Attributes, CreationAttributes } from 'sequelize';
+import { Model } from 'sequelize-typescript';
+
 export interface IUnfilledAtt {
   createdAt: Date;
   updatedAt: Date;
@@ -11,3 +14,7 @@ export interface IUnfilledNotParanoidAtt {
 export type Optional<T> = {
   [P in keyof T]?: T[P] | null;
 };
+
+export type RelationAttribute<M extends Model, T = 'attributes', ForeignKey extends string = ''> = T extends 'attributes'
+  ? Attributes<M>
+  : Omit<CreationAttributes<M>, ForeignKey>;
