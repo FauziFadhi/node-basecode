95d75fbfebbbb68548fa19a133a004128a7adaf7
Fauzi Fadhillah <fauzifadhi@rollingglory.com>
2023-03-16 13:14:08 +0700

    depcrated(decorator): user decorator deprecated

--- src/utils/decorators/user.decorator.ts
+++ src/utils/decorators/user.decorator.ts
@@ -1,5 +1,8 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
+/**
+ * @deprecated you might need to use @AuthenticatedUser and CLSService from nestjs-cls
+ */
 export const User = createParamDecorator((data: any, ctx: ExecutionContext) => {
   const request = ctx.switchToHttp().getRequest();
   return request.user;
